90637be368c2bebe1b5268ad98014d4938ea4269
Chet Kener <Cl3Kener@gmail.com>
2015-10-29 19:17:06 -0400

    Show UBER in Recovery
    * Also silence the stuff about Target.  That's boring
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -582,8 +582,18 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
   # Dump fingerprints
-  script.Print("Target: %s" % CalculateFingerprint(
-      oem_props, oem_dict, OPTIONS.info_dict))
+  #script.Print("Target: %s" % CalculateFingerprint(
+  #    oem_props, oem_dict, OPTIONS.info_dict))
+  script.Print("******************************************");
+  script.Print("*                                        *");
+  script.Print("* _|    _|  _|_|_|    _|_|_|_|  _|_|_|   *");
+  script.Print("* _|    _|  _|    _|  _|        _|    _| *");
+  script.Print("* _|    _|  _|_|_|    _|_|_|    _|_|_|   *");
+  script.Print("* _|    _|  _|    _|  _|        _|    _| *");
+  script.Print("* _|    _|  _|    _|  _|        _|    _| *");
+  script.Print("*   _|_|    _|_|_|    _|_|_|_|  _|    _| *");
+  script.Print("*                                        *");
+  script.Print("******************************************"); 
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
